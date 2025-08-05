8bf95617c4269eeec24c7fd2107929820dc53c0c
psion <psion@Predator>
2025-08-04 23:47:08 -0400

    Resets on version update

--- scripts/Reset.js
+++ scripts/Reset.js
@@ -1,14 +1,27 @@
 let reset = document.getElementById("reset");
-reset.addEventListener("click", function(){
-	if(confirm("Reset Game State?")){
-		countries = null;
-		setupCards();
-		document.getElementById("tabTerritory").click();
-		
-		numDiceToRollSetup = 2;//set Default
-		localStorage.setItem("numDiceToRoll", numDiceToRollSetup);
-		document.getElementById("diceRadio2").checked = true;
-		
-		calcProduction();
+reset.addEventListener("click", function() {
+	if (confirm("Reset Game State?")) {
+		resetState();
 	}
 });
+
+function resetState() {
+	countries = null;
+	setupCards();
+	document.getElementById("tabTerritory").click();
+
+	numDiceToRollSetup = 2;//set Default
+	localStorage.setItem("numDiceToRoll", numDiceToRollSetup);
+	document.getElementById("diceRadio2").checked = true;
+
+	calcProduction();
+
+}
+
+let version = 2;
+
+let storedVersion = localStorage.getItem("version");
+
+if (storedVersion != version) {
+	resetState();
+}
--- scripts/Save.js
+++ scripts/Save.js
@@ -1,4 +1,5 @@
 function saveGameState() {
 	let savedData = JSON.stringify(countries);
 	localStorage.setItem("countrySetup", savedData);
+	localStorage.setItem("version", 1);
 }
