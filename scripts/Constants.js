9dc22eea89a2a28e0ccaef02a694215a495a6346
psion <psion@Predator>
2025-08-09 11:42:46 -0400

    new constants

--- scripts/Constants.js
+++ scripts/Constants.js
@@ -1,5 +1,6 @@
 const FIGHTING = '\u{2694}'; // ⚔
 const DIE = '\u{1F3B2}'; // 🎲 
-const DOUBLE_ARROW =  '\u{21C6}' // ⇆ 
+const PAIRED_ARROW =  '\u{21C6}' // ⇆ 
+const ARROW_LEFT = '\u{219E}'; //  ↞
+const ARROW_RIGHT = '\u{21A0}'; // ↠
 const SILHOUETTE =  '\u{1F464}' // 👤 
-
